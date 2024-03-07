2f85fbc41efc618c81d391f58e4576999df0f594
22DP3AErcm <ae00170@rvt.lv>
2024-03-07 21:24:05 +0200

    gitignore

--- src/main/java/org/openjfx/Login.java
+++ src/main/java/org/openjfx/Login.java
@@ -2,8 +2,22 @@ package org.openjfx;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Hyperlink;
+import javafx.event.ActionEvent;
 
 public class Login {
+
+    @FXML
+    private Hyperlink myHyperlink;
+
+    @FXML
+    void handleHyperlinkAction(ActionEvent event) {
+        try {
+            switchToRegister();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @FXML
     private void switchToRegister() throws IOException {
