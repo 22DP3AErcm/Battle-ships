7451a76848b40c0822f72a418e673ba6d20e9b44
22DP3AErcm <131261272+22DP3AErcm@users.noreply.github.com>
2024-05-04 11:25:04 +0300

    test class

--- src/test/java/org/openjfx/Testgame.java
+++ src/test/java/org/openjfx/Testgame.java
@@ -5,7 +5,15 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Testgame {
     @Test
-    public void test() {
-        assertEquals(1, 1);
+    public void Gridtest() {
+        Enemy enemy = new Enemy();
+        int[] cordinates = {01, 02, 03};
+        assertEquals(true, enemy.validShipPlacement(cordinates));
+    }
+    @Test
+    public void ShipPlacement() {
+        Enemy enemy = new Enemy();
+        int[] cordinates = {99, 02, 03};
+        assertEquals(false, enemy.validShipPlacement(cordinates));
     }
 }
