63c57d033b4eb1384461bfed671e46dbf58017b6
22DP3AErcm <131261272+22DP3AErcm@users.noreply.github.com>
2024-05-07 20:02:47 +0300

    TEST

--- src/test/java/org/openjfx/Testgame.java
+++ src/test/java/org/openjfx/Testgame.java
@@ -3,6 +3,7 @@ package org.openjfx;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+
 public class Testgame {
     @Test
     public void Gridtest() {
@@ -17,4 +18,28 @@ public class Testgame {
         int[] cordinates = {99, 02, 03};
         assertEquals(false, enemy.validShipPlacement(cordinates));
     }
-}
+
+    @Test
+    public void enemyShipGeneration() {
+        Enemy enemy = new Enemy();
+        enemy.generateEnemyShips();
+        assertEquals(9, enemy.enemyShips.size());
+    }
+
+    @Test
+    public void convertCordinates() {
+        Enemy enemy = new Enemy();
+        String cordinates = "B3";
+        int[] cordinatesArray = enemy.convertCoordinate(cordinates);
+        assertEquals(1, cordinatesArray[0]);
+        assertEquals(2, cordinatesArray[1]);
+    }
+    
+    @Test
+    public void convertCordinates2() {
+        Enemy enemy = new Enemy();
+        int[] cordinates = {12};
+        String[] cordinatesString = enemy.convertCoordinate(cordinates);
+        assertEquals("A2", cordinatesString[0]);
+    }
+} 
